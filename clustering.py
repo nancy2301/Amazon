be9931706548f69d4b1e22486caa90591cc617dc
eirik <fosnaeseirik@seattleu.edu>
2019-08-03 16:02:08 -0700

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -4,7 +4,6 @@ Created on Thu Aug  1 15:35:23 2019
 
 @author: eirik
 """
-
 import os
 
 os.chdir("C:/Users/eirik/OneDrive/Documents/Seattle U classes/Summer 2019/MGMT 5200/Amazon")
@@ -16,7 +15,6 @@ from sklearn.cluster import KMeans
 from sklearn import metrics
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report
-import csv
 
 master = pd.read_csv("master.csv", sep=',', header=0)
 
@@ -33,7 +31,7 @@ labels = master["tools_used_not"]
 master['tool_used_not[Yes]'] = master['tools_used_not'].astype('category').cat.codes
 X = master.drop(['Unnamed: 0', 'Month', 'Geo_Code', 'billing_month', 'Customer_ID', 'tools_used_not', 'Visualize', 'Alert', 'Report', 'Tools', 'tool_used_not[Yes]', ], axis = 1)
 
-clustering_kmeans = KMeans(n_clusters= 4)
+clustering_kmeans = KMeans(n_clusters= 5)
 master['clusters'] = clustering_kmeans.fit_predict(X)
 
 master.to_csv('master_clust.csv')
@@ -47,17 +45,18 @@ grouped_data['totalBilled'].aggregate(np.mean).reset_index()
 x = master.drop(['Unnamed: 0', 'billing_month', 'Customer_ID', 'tools_used_not', 'totalBilled', 'Visualize', 'Alert', 'Report', 'Tools', 'tool_used_not[Yes]'], axis = 1)
 
 X_train = x[x['Month'] != 'June-19']
-y_train = master[x['Month'] != 'June-19']
+y_train = master[master['Month'] != 'June-19']
 
 X_test = x[x['Month'] == 'June-19']
-y_test = master[x['Month'] == 'June-19']
+y_test = master[master['Month'] == 'June-19']
 
-X_train = x.drop(['Month'], axis = 1)
+X_train = X_train.drop(['Month'], axis = 1)
 
-X_test = x.drop(['Month'], axis = 1)
+X_test = X_test.drop(['Month'], axis = 1)
 
-y_train = master['tools_used_not']
-y_test = master['tools_used_not']
+y_train = y_train['tools_used_not']
+y_test = y_test['tools_used_not']
+
 
 mlp = MLPClassifier(hidden_layer_sizes=(10), max_iter=1000, activation = 'logistic')  
 mlp.fit(X_train, y_train)  
@@ -69,7 +68,12 @@ y_pred = mlp.predict(X_test)
 print('')
 print('NN Model - one hidden layer - 10 nodes, Logistic')
 print(metrics.confusion_matrix(y_test, y_pred))
-print("Accuracy", metrics.accuracy_score(y_test, y_pred))
+
+print("Neural Network Test Accuracy:",metrics.accuracy_score(y_test, y_pred_test))
+print("F score:", metrics.f1_score(y_test,y_pred_test,average = 'weighted'))
+print("Precision: ", metrics.precision_score(y_test,y_pred_test,average = 'weighted'))
+print("Recall:", metrics.recall_score(y_test,y_pred_test,average = 'weighted'))
+
 
 # With activation identity
 mlp = MLPClassifier(hidden_layer_sizes=(10), max_iter=1000, activation = 'identity')  
@@ -82,17 +86,7 @@ y_pred = mlp.predict(X_test)
 print('')
 print('NN Model - one hidden layer - 10 nodes, Identity')
 print(metrics.confusion_matrix(y_test, y_pred)) 
-print("Accuracy", metrics.accuracy_score(y_test, y_pred))
 
-
-y_pred_train = mlp.predict(X_train)   ## predict train set
-y_pred_test = mlp.predict(X_test)     ## predict test set
-print(metrics.confusion_matrix(y_test, y_pred_test))
-print("Neural Network Train Accuracy:",metrics.accuracy_score(y_train, y_pred_train))
-print("Neural Network Test Accuracy:",metrics.accuracy_score(y_test, y_pred_test))
-print("F score:", metrics.f1_score(y_test,y_pred_test,average = 'weighted'))
-print("Precision: ", metrics.precision_score(y_test,y_pred_test,average = 'weighted'))
-print("Recall:", metrics.recall_score(y_test,y_pred_test,average = 'weighted'))
 
 
 # with activation tanh
@@ -109,6 +103,7 @@ print(metrics.confusion_matrix(y_test, y_pred))
 print("Accuracy", metrics.accuracy_score(y_test, y_pred))
 
 
+
 # with activation relu
 mlp = MLPClassifier(hidden_layer_sizes=(10), max_iter=1000, activation = 'relu')  
 mlp.fit(X_train, y_train)  
@@ -120,22 +115,11 @@ y_pred = mlp.predict(X_test)
 print('')
 print('NN Model - one hidden layer - 10 nodes, relu')
 print(metrics.confusion_matrix(y_test, y_pred)) 
-print("Accuracy", metrics.accuracy_score(y_test, y_pred))
 
-clf = clf.fit(X_train, y_train)
-
-y_pred_train = clf.predict(X_train)   ## predict train set
-y_pred_test = clf.predict(X_test)     ## predict test set
-print(metrics.confusion_matrix(y_test, y_pred_test))
-print("Decision Tree Train Accuracy:",metrics.accuracy_score(y_train, y_pred_train))
-print("Decision Tree Test Accuracy:",metrics.accuracy_score(y_test, y_pred_test))
-print("F score:", metrics.f1_score(y_test,y_pred_test,average = 'weighted'))
-print("Precision: ", metrics.precision_score(y_test,y_pred_test,average = 'weighted'))
-print("Recall:", metrics.recall_score(y_test,y_pred_test,average = 'weighted'))
 
 
 # with three layers and less nodes
-mlp = MLPClassifier(hidden_layer_sizes=(3, 3, 3), max_iter=1000)  
+mlp = MLPClassifier(hidden_layer_sizes=(3, 3), max_iter=1000)  
 mlp.fit(X_train, y_train)  
 
 ## predict test set 
@@ -148,12 +132,58 @@ print(metrics.confusion_matrix(y_test, y_pred))
 print("Accuracy", metrics.accuracy_score(y_test, y_pred))
 
 
-from sklearn.naive_bayes import GaussianNB
-gnb.fit(X_train, y_train)
-y_pred = gnb.predict(X_test)
-print(metrics.confusion_matrix(y_test, y_pred))
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-print(metrics.classification_report(y_test, y_pred))
+# with three layers and less nodes
+mlp = MLPClassifier(hidden_layer_sizes=(3, 3), max_iter=1000)  
+mlp.fit(X_train, y_train)  
+
+## predict test set 
+y_pred = mlp.predict(X_test)  
+
+## confusion matrix
+print('')
+print('NN Model - three hidden layer - 3 nodes')
+print(metrics.confusion_matrix(y_test, y_pred)) 
+print("Accuracy", metrics.accuracy_score(y_test, y_pred))
+
+
+
+# With activation identity
+mlp = MLPClassifier(hidden_layer_sizes=(8), max_iter=1000, activation = 'identity')  
+mlp.fit(X_train, y_train)  
+
+## predict test set 
+y_pred = mlp.predict(X_test)  
+
+## confusion matrix
+print('')
+print('NN Model - one hidden layer - 8 nodes, Identity')
+print(metrics.confusion_matrix(y_test, y_pred)) 
+
+
+
+# With activation identity
+mlp = MLPClassifier(hidden_layer_sizes=(7), max_iter=1000, activation = 'identity')  
+mlp.fit(X_train, y_train)  
+
+## predict test set 
+y_pred = mlp.predict(X_test)  
+
+## confusion matrix
+print('')
+print('NN Model - one hidden layer - 7 nodes, Identity')
+print(metrics.confusion_matrix(y_test, y_pred)) 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
