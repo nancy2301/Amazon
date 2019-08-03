7a0f7f75ee6383aaa2ad9e1cf71fa9a8cc9017d0
eirik <fosnaeseirik@seattleu.edu>
2019-08-03 13:22:35 -0700

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -14,13 +14,9 @@ import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn import metrics
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC 
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.preprocessing import LabelEncoder
-from sklearn import preprocessing 
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report
+import csv
 
 master = pd.read_csv("master.csv", sep=',', header=0)
 
@@ -35,10 +31,18 @@ master.replace(cleannum1, inplace = True)
 
 labels = master["tools_used_not"]
 master['tool_used_not[Yes]'] = master['tools_used_not'].astype('category').cat.codes
-X = master.drop(['Unnamed: 0', 'Month', 'billing_month', 'Customer_ID', 'tools_used_not', 'Visualize', 'Alert', 'Report', 'Tools', 'tool_used_not[Yes]', ], axis = 1)
+X = master.drop(['Unnamed: 0', 'Month', 'Geo_Code', 'billing_month', 'Customer_ID', 'tools_used_not', 'Visualize', 'Alert', 'Report', 'Tools', 'tool_used_not[Yes]', ], axis = 1)
 
-clustering_kmeans = KMeans(n_clusters= 6)
+clustering_kmeans = KMeans(n_clusters= 4)
 master['clusters'] = clustering_kmeans.fit_predict(X)
+
+master.to_csv('master_clust.csv')
+
+grouped_data = master.groupby(['clusters'])
+
+grouped_data['Customer_size'].aggregate(np.mean).reset_index()
+grouped_data['ProductsUsed'].aggregate(np.mean).reset_index()
+grouped_data['totalBilled'].aggregate(np.mean).reset_index()
 
 x = master.drop(['Unnamed: 0', 'billing_month', 'Customer_ID', 'tools_used_not', 'totalBilled', 'Visualize', 'Alert', 'Report', 'Tools', 'tool_used_not[Yes]'], axis = 1)
 
@@ -81,6 +85,16 @@ print(metrics.confusion_matrix(y_test, y_pred))
 print("Accuracy", metrics.accuracy_score(y_test, y_pred))
 
 
+y_pred_train = mlp.predict(X_train)   ## predict train set
+y_pred_test = mlp.predict(X_test)     ## predict test set
+print(metrics.confusion_matrix(y_test, y_pred_test))
+print("Neural Network Train Accuracy:",metrics.accuracy_score(y_train, y_pred_train))
+print("Neural Network Test Accuracy:",metrics.accuracy_score(y_test, y_pred_test))
+print("F score:", metrics.f1_score(y_test,y_pred_test,average = 'weighted'))
+print("Precision: ", metrics.precision_score(y_test,y_pred_test,average = 'weighted'))
+print("Recall:", metrics.recall_score(y_test,y_pred_test,average = 'weighted'))
+
+
 # with activation tanh
 mlp = MLPClassifier(hidden_layer_sizes=(10), max_iter=1000, activation = 'tanh')  
 mlp.fit(X_train, y_train)  
@@ -107,6 +121,17 @@ print('')
 print('NN Model - one hidden layer - 10 nodes, relu')
 print(metrics.confusion_matrix(y_test, y_pred)) 
 print("Accuracy", metrics.accuracy_score(y_test, y_pred))
+
+clf = clf.fit(X_train, y_train)
+
+y_pred_train = clf.predict(X_train)   ## predict train set
+y_pred_test = clf.predict(X_test)     ## predict test set
+print(metrics.confusion_matrix(y_test, y_pred_test))
+print("Decision Tree Train Accuracy:",metrics.accuracy_score(y_train, y_pred_train))
+print("Decision Tree Test Accuracy:",metrics.accuracy_score(y_test, y_pred_test))
+print("F score:", metrics.f1_score(y_test,y_pred_test,average = 'weighted'))
+print("Precision: ", metrics.precision_score(y_test,y_pred_test,average = 'weighted'))
+print("Recall:", metrics.recall_score(y_test,y_pred_test,average = 'weighted'))
 
 
 # with three layers and less nodes
