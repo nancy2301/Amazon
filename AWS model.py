9205f65ab3e2142fb830d15b25a485104b4ff5b9
a-fanfan <32152130+a-fanfan@users.noreply.github.com>
2019-08-04 15:58:45 -0700

    7 class

--- AWS model.py
+++ AWS model.py
@@ -12,10 +12,12 @@ import patsy
 import graphviz
 
 os.chdir("/Users/xiangyifan/Desktop/SU/MSBA/19RQ/MGMT5200/Project/AMAZOn")
-All = pd.read_csv("All.csv", sep = ',', header = 0)
+All = pd.read_csv("Master.csv", sep = ',', header = 0)
 
+All = All[All['tools_used_not'] == 'Yes']
 
 # divide df into testing df and training df
+
 
 All_train = All[All.Month != 'June-19']
 All_test = All[All.Month == 'June-19']
@@ -26,7 +28,7 @@ y_test = All_test['Tools']
 
 ## Decision Tree
 
-col_names = ['Customer_size', 'Geo_Code', 'ProductsUsed', 'totalBilled', 'customer_age']     ## variable selected
+col_names = ['Customer_size', 'Geo_Code', 'ProductsUsed', 'totalBilled', 'customer_age','cluster']     ## variable selected
 tree_train = All_train[col_names]
 tree_test = All_test[col_names]
 
@@ -57,8 +59,9 @@ print("Recall:", metrics.recall_score(y_test,y_pred_test,average = 'weighted'))
 dot_data = tree.export_graphviz(clf, out_file='Dtree.dot',filled=True,rounded = True, feature_names=X_train.columns)
 
 
+
 # MNLogit Model
-interaction2 = "Tools ~ Customer_size + Geo_Code + ProductsUsed + totalBilled + customer_age "
+interaction2 = "Tools ~ Customer_size + Geo_Code + ProductsUsed + totalBilled + customer_age + cluster "
 
 y_train,XX2train = patsy.dmatrices(interaction2, All_train, return_type = "dataframe")
 
@@ -84,3 +87,19 @@ print("MNLogit Test Accuracy:",metrics.accuracy_score(y_test, y_pred_test))
 print("F score:", metrics.f1_score(y_test,y_pred_test,average = 'weighted'))
 print("Precision: ", metrics.precision_score(y_test,y_pred_test,average = 'weighted'))
 print("Recall:", metrics.recall_score(y_test,y_pred_test,average = 'weighted'))
+
+
+
+## Neural Network
+mlp = MLPClassifier(hidden_layer_sizes=(30,30,30), random_state=109,max_iter=5000)
+mlp.fit(X_train, y_train)
+y_pred_train = mlp.predict(X_train)    ## predict train set
+y_pred_test = mlp.predict(X_test)  	## predict test set
+print(metrics.confusion_matrix(y_test, y_pred_test))
+print("Neural Network Train Accuracy:",metrics.accuracy_score(y_train, y_pred_train))
+print("Neural Network Test Accuracy:",metrics.accuracy_score(y_test, y_pred_test))
+print("F score:", metrics.f1_score(y_test,y_pred_test,average = 'weighted'))
+print("Precision: ", metrics.precision_score(y_test,y_pred_test,average = 'weighted'))
+print("Recall:", metrics.recall_score(y_test,y_pred_test,average = 'weighted'))
+
+
