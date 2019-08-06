b05c278fd2979dc7366529e37dd56496de692c07
Ziyu <jinziyu1116@gmail.com>
2019-08-05 19:39:41 -0700

    Tested

--- Ready to Use Code/Step_2.py
+++ Ready to Use Code/Step_2.py
@@ -10,13 +10,9 @@ Created on Sun Aug  4 18:49:45 2019
 #import os
 #os.chdir("C:/Users/eirik/OneDrive/Documents/Seattle U classes/Summer 2019/MGMT 5200/Amazon")
 
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import classification_report
-from sklearn import metrics
 from sklearn import tree
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split
@@ -43,7 +39,7 @@ def cleanData(master):
     return(master)
 
     
-def prepare(master):
+def prepare_master(master):
     master['tool_used_not[Yes]'] = master['tools_used_not'].astype('category').cat.codes
     X = master.drop(['Unnamed: 0', 'Month', 'Customer_ID', 'Customer_size', 'tools_used_not', 'Visualize', 'Alert', 'Report', 'Tools', 'tool_used_not[Yes]'], axis = 1)
     scaler = StandardScaler()  
@@ -51,6 +47,15 @@ def prepare(master):
     X = scaler.transform(X)
     
     return(X)
+    
+def prepare_new(new):
+    X = new.drop(['Unnamed: 0', 'Month', 'Customer_ID', 'Customer_size', 'Visualize', 'Alert', 'Report'], axis = 1)
+    scaler = StandardScaler()  
+    scaler.fit(X)
+    X = scaler.transform(X)
+    
+    return(X)
+    
     
 def model_Cluster(X, X_new): 
     clustering_kmeans = KMeans(n_clusters= 5, random_state = 74)
@@ -63,9 +68,9 @@ def model_Cluster(X, X_new):
 def model_yes_no(master, new):   
     # Prepare data
     x = master.drop(['Unnamed: 0', 'Customer_ID', 'tools_used_not', 'Visualize', 'Alert', 'Report', 'Tools', 'tool_used_not[Yes]'], axis = 1)
-    x_new = new.drop(['Unnamed: 0', 'Customer_ID', 'tools_used_not', 'Visualize', 'Alert', 'Report', 'Tools', 'tool_used_not[Yes]'], axis = 1)
+    x_new = new[["Customer_size", "Geo_Code", "ProductsUsed", "totalBilled", "customer_age", "clusters"]]
     
-    # Get training dataset
+    # Get training dataset and prepare data
     X_train = x[x['Month'] != 'June-19']
     y_train = master[master['Month'] != 'June-19']
     
@@ -73,8 +78,6 @@ def model_yes_no(master, new):
     
     y_train = y_train['tools_used_not']
     
-    x_new = x_new.drop(['Month'], axis = 1)
-
     # Train the model 
     mlp = MLPClassifier(hidden_layer_sizes=(8), max_iter=1000, activation = 'identity', random_state = 9)  
     mlp.fit(X_train, y_train)  
@@ -113,23 +116,18 @@ def model_Visualize(All, new):
     num_col_names = ['totalBilled','customer_age']   ## scale only numeric variable
     scaler = StandardScaler().fit(X_train[num_col_names].values)
     X_train[num_col_names] = scaler.transform(X_train[num_col_names].values)
-    X_new[num_col_names] = scaler.transform(X_new[num_col_names].values)
-   
+    X_new[num_col_names] = scaler.transform(X_new[num_col_names].values) 
 
     # Run the model
     Logit = sm.MNLogit(y_train, X_train).fit_regularized()
     X_new["Intercept"] = 1
-    
-    print(X_new.info())
-    print(X_train.info())
+
     y_pred_prob = Logit.predict(X_new)
-    y_pred = y_vis.astype('category').cat.categories[y_pred_prob.idxmax(axis=1)]
-    
+    y_pred = y_vis.astype('category').cat.categories[y_pred_prob.idxmax(axis=1)]    
     
     return(y_pred)
     
 def model_Alert(All, new):
-    
     # Prepare data   
     y_vis = All['Alert']
     X = All[['Geo_Code','ProductsUsed','customer_age','totalBilled','clusters','Customer_size']]    
@@ -155,8 +153,7 @@ def model_Report(All, new):
     # Prepare data   
     y_vis = All['Report']
     X = All[['Geo_Code','ProductsUsed','customer_age','totalBilled','clusters','Customer_size']]    
-    X_new = new[['Geo_Code','ProductsUsed','customer_age','totalBilled','clusters','Customer_size']]  
-    
+    X_new = new[['Geo_Code','ProductsUsed','customer_age','totalBilled','clusters','Customer_size']]    
 
     # Get training data             
     X = pd.get_dummies(X,drop_first=True,prefix_sep='_')
@@ -170,8 +167,7 @@ def model_Report(All, new):
     
     # Run the model
     y_pred = clf.predict(X_new)     
-    
-    
+     
     return(y_pred)
 
 
@@ -184,10 +180,10 @@ def Main():
     # Clean datasets
     master = cleanData(master)
     new = cleanData(new)
-       
+        
     # Add clusters to datasets
-    master["clusters"] = model_Cluster(prepare(master),prepare(master))
-    new["clusters"] = model_Cluster(prepare(new),prepare(new))
+    master["clusters"] = model_Cluster(prepare_master(master),prepare_master(master))
+    new["clusters"] = model_Cluster(prepare_new(new),prepare_new(new))
 
     # Predict whether customers will use any of the products
     new["Yes_No_Prediction"] = model_yes_no(master, new)
@@ -204,11 +200,11 @@ def Main():
     # Report
     new_yes["Report_Prediction"] = model_Report(master_yes, new_yes)
     
+    # Write results
     new.to_csv( "new_yes_no_result.csv", index=False, encoding='utf-8-sig')
     new_yes.to_csv( "new_product_result.csv", index=False, encoding='utf-8-sig')
     
+ 
     
-
-
 if __name__ == "__main__":
     Main()
